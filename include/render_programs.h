c9d2deaa011546884c307124c55ef6dff6e14dbf
GigaTortilla <martinappel01@gmail.com>
2024-12-26 12:00:32 +0100

    changed structure to quickly switch between rendering functions

---
+++ include/render_programs.h
@@ -0,0 +1,13 @@
+//
+// Created by Martin Appel on 26.12.24.
+//
+
+#ifndef RENDER_PROGRAMS_H
+#define RENDER_PROGRAMS_H
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+int triangles();
+
+#endif //RENDER_PROGRAMS_H

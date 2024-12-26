79e8cc8faf9126f3aa0114daeb560733d566118f
GigaTortilla <martinappel01@gmail.com>
2024-12-26 11:46:48 +0100

    triangle tests

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,8 @@ int main() {
     GLFWwindow *window = init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Rendering");
 
     // create shader program from vertex and fragment shader
-    unsigned int shader_program = build_program("shader.vert", "shader.frag");
-    unsigned int shader_green = build_program("shader.vert", "green.frag");
-
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-       -0.5f, -0.5f, 0.0f,  // bottom left
-       -0.5f,  0.5f, 0.0f   // top left
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
+    unsigned int shader_program = build_program("tri.vert", "tri.frag");
+    unsigned int shader_green = build_program("tri.vert", "green_tri.frag");
 
     float triangle1[] = {
         -0.55f, -0.3f, 0.0f,
@@ -38,15 +27,24 @@ int main() {
          0.30f,  0.3f, 0.0f
     };
 
+    // create vertex buffer objects and vertex array objects
     unsigned int VBOs[2], VAOs[2];
     glGenVertexArrays(2, VAOs);
     glGenBuffers(2, VBOs);
 
+    // 1. bind the VAO
+    // 2. bind VBO and specify vertex buffer data (the object to render)
     glBindVertexArray(VAOs[0]);
     glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
+    // 3. specify the values / attributes sent to the shader (how the VBO is organized)
+    // index - gets incremented for each layout location / variable in shader
+    // size - number of values (3 for vec3 and so on)
+    // stride - amount of occupied memory (distance between attributes)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    // enable specified attributes for following index
     glEnableVertexAttribArray(0);
+    // unbind VAO
     glBindVertexArray(0);
 
     glBindVertexArray(VBOs[1]);
@@ -69,15 +67,18 @@ int main() {
         double timeValue = glfwGetTime();
         auto red = static_cast<float>(0.5f * sin(timeValue * 5.0f) + 0.5f);
 
+        // background color^:w
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // first triangle
         glUseProgram(shader_program);
         glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glUniform4f(vertex_color_loc, red, 0.1f, 0.1f, 1.0f);
+        glUniform4f(vertex_color_loc, red, 1.0f - red, 0.1f, 1.0f);
 
+        // second triangle
         glUseProgram(shader_green);
         glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -86,9 +87,11 @@ int main() {
         glfwPollEvents();
     }
 
+    // cleanup after use
     glDeleteVertexArrays(2, VAOs);
     glDeleteBuffers(2, VBOs);
     glDeleteProgram(shader_program);
+    glDeleteProgram(shader_green);
 
     glfwTerminate();
     return EXIT_SUCCESS;

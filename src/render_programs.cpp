df5be2ecf28ebbdd9738d4411fd635df6e4f6f15
GigaTortilla <martinappel01@gmail.com>
2024-12-30 17:23:45 +0100

    glm tests

--- src/render_programs.cpp
+++ src/render_programs.cpp
@@ -27,43 +27,13 @@ int cubes() {
     // initialize GLFW window
     GLFWwindow *window = init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Rendering");
 
-    // create shader program from vertex and fragment shader
-    unsigned int shader_program = build_program("tri_flip.vert", "tri_variable.frag");
-
-    // triangle coordinates
-    float triangle1[] = {
-        -0.55f, -0.3f, 0.0f,
-        -0.05f, -0.3f, 0.0f,
-        -0.30f,  0.3f, 0.0f
-    };
-
     double last_frame = 0.0;
 
-    // create vertex buffer objects and vertex array objects
-    unsigned int VBOs[4], VAOs[4];
-    glGenVertexArrays(4, VAOs);
-    glGenBuffers(4, VBOs);
-
-    // first triangle
-    // 1. bind the VAO
-    // 2. bind VBO and specify vertex buffer data (the object to render)
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
-    // 3. specify the values / attributes sent to the shader (how the VBO is organized)
-    // index - gets incremented for each layout location / variable in shader
-    // size - number of values (3 for vec3 and so on)
-    // stride - amount of occupied memory (distance between attributes)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-    // enable specified attributes for following index
-    glEnableVertexAttribArray(0);
-    // unbind VAO
-    glBindVertexArray(0);
-
-    glUseProgram(shader_program);
-
-    // getting the location of the vertex color uniform in the fragment shader
-    int vertex_color_loc = glGetUniformLocation(shader_program, "redVal");
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    auto trans = glm::mat4(1.0f); // identity matrix
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << ", " << vec.y << ", " << vec.z << std::endl;
 
     /**
      * Wireframe Mode
@@ -81,27 +51,13 @@ int cubes() {
         double delta_time = time_value - last_frame;
         last_frame = time_value;
 
-        auto red = static_cast<float>(0.5f * sin(time_value * 5.0f) + 0.5f);
-
         // background color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // first triangle
-        glUseProgram(shader_program);
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glUniform4f(vertex_color_loc, red, 1.0f - red, 0.1f, 1.0f);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    // cleanup after use
-    glDeleteVertexArrays(4, VAOs);
-    glDeleteBuffers(4, VBOs);
-    glDeleteProgram(shader_program);
 
     glfwTerminate();
     return EXIT_SUCCESS;
